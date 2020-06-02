ec89d9d84a5648db2e8109d8ab59305c229cca43
sitayebsofiane <sitayebsofiane51@gmail.com>
2020-06-02 16:44:47 +0200

    config bdd

--- inc/bdd.php
+++ inc/bdd.php
@@ -1,10 +1,13 @@
 <?php
-
+$host = "localhost";
+$db_name = "clientchat";
+$username = "postgres";
+$password = "as122014";
 /**
  * Connexion à la BDD ,je utilse postgresql
  */
 try{
-	$db = new PDO("pgsql:host=localhost;dbname=clientchat","postgres","as122014");
+	$db = new PDO("pgsql:host=".$host.";dbname=".$db_name,$username,$password);
 }catch(PDOException $e){
 	die('Erreur : ' . $e->getMessage());
 }
